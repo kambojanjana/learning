3f04d9911a38ecdaf6367df83e3784c805c398ff
kambojanjana <88776712+kambojanjana@users.noreply.github.com>
2023-03-28 01:01:53 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Test {
+	public static void main(String[] args) {
+		String str = "vancouverr";
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		for (int i = 0; i < str.length(); i++) {
+			
+			char key = str.charAt(i);//i =0, v; i= 1, a;
+			
+			if (map.containsKey(key)) {//// map : {v : 2} ; if map.containsKey(v)
+				
+				int value_old = map.get(key);// map : {v : 2}
+				
+				map.put(key, value_old + 1);// map : {v : 2 + 1}
+				
+				System.out.println(key );// map : {v:3}
+				
+			} else {
+				map.put(key, 1);
+			}
+		}
+		
+//System.out.println(map);
+	}
+
+}
